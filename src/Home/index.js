07c07f4a210fc4010181a6228adbf37b94981869
Jeyabalaji <jeyabalaji.tm@grabtaxi.com>
2023-09-25 23:31:59 +0530

    updated json location

--- src/Home/index.js
+++ src/Home/index.js
@@ -3,18 +3,34 @@ import { getMemberStatus } from "../service/api";
 import "./index.scss";
 const Home = () => {
   const [membersByRank, setMembersByRank] = useState([]);
+  const [updatedAt, setUpdatedAt] = useState("");
 
-  useEffect(() => {
+  const getData = () => {
     getMemberStatus().then((data) => {
       setMembersByRank(data?.members.sort((a, b) => a.rank - b.rank));
+      setUpdatedAt(data?.time);
     });
+  };
+  useEffect(() => {
+    getData();
   }, []);
+  const refresh = () => {
+    getData();
+  };
   return (
     <div className="home">
-      <div className="members-heading">
-        <div className="number">எண்</div>
-        <div className="name">பெயர்</div>
-        <div className="rank">நிலவரம்</div>
+      <div className="home-header">
+        <div className="members-heading">
+          <div className="number">எண்</div>
+          <div className="name">பெயர்</div>
+          <div className="rank">நிலவரம்</div>
+        </div>
+        <div className="updated-at">
+          <div>Updated At: {updatedAt}</div>
+          <div className="refresh-button" onClick={refresh}>
+            Refresh
+          </div>
+        </div>
       </div>
       {membersByRank.map((member) => {
         return (
