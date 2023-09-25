83bc19408a8a4d96f9589ec59496aea0e338f6f1
Jeyabalaji <jeyabalaji.tm@grabtaxi.com>
2023-09-25 23:32:45 +0530

    changed url

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,12 @@
 export const getMemberStatus = () => {
   const isLocal = window.location.host.includes("localhost");
-  return fetch(isLocal? "memberStatus.json" :"https://raw.githubusercontent.com/antandbuffalo/spd-election/main/src/Home/memberStatus.json")
+  return fetch(
+    isLocal
+      ? "memberStatus.json"
+      : "https://raw.githubusercontent.com/antandbuffalo/spd-election/main/public/memberStatus.json"
+  )
     .then((response) => response.json())
     .then((data) => {
       return data;
     });
-}
+};
