6206060fc4f706441afecf2d9e7601b238224e5f
Jeyabalaji <jeyabalaji.tm@grabtaxi.com>
2023-09-26 17:52:02 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Home from "./Home";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">சு பெ தேவஸ்தானம் தேர்தல் முடிவுகள் 2023</header>
+      <header className="App-header">
+        சு பெ தேவஸ்தானம் தேர்தல் முடிவுகள் 2023
+      </header>
       <Home />
+      <footer>
+        <div className="footer">Brought to you by Jeyabalaji</div>
+      </footer>
     </div>
   );
 }
