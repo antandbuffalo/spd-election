565098059e5c0f03becd954479085576ea756170
Jeyabalaji <jeyabalaji.tm@grabtaxi.com>
2023-09-28 18:33:28 +0530

    first version

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 export const getMemberStatus = async () => {
   // const isLocal = window.location.host.includes("localhost");
   try {
-    const response = await fetch("memberStatus.json");
+    const response = await fetch(`memberStatus.json?time=${new Date().getTime()}`);
     const json = await response.json();
     return json;
   } catch (e) {
