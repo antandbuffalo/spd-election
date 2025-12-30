b73a55b75efa240eebafb9f0c9f74d9284da9eb9
Jeyabalaji <balaji.jeya@gmail.com>
2025-12-30 15:45:48 +0530

    added protected route

--- src/AppRoutes/index.js
+++ src/AppRoutes/index.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import App from "../App";
 import Review from "../Review";
 import ReviewList from "../ReviewList";
@@ -8,6 +8,17 @@ import Disclaimer from "../Disclaimer";
 import ContactUs from "../ContactUs";
 import HostAnalytics from "../HostAnalytics/HostAnalytics";
 import Dashboard from "../Admin/Dashboard/Dashboard";
+import { useAuth } from "../firebase/AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to={APP_ROUTES.login} replace />;
+  }
+
+  return children;
+};
 
 const AppRoutes = () => {
   return (
@@ -16,7 +27,14 @@ const AppRoutes = () => {
         <Route path={APP_ROUTES.home} element={<App />} />
         <Route path={APP_ROUTES.review} element={<Review />} />
         <Route path={APP_ROUTES.reviewList} element={<ReviewList />} />
-        <Route path={APP_ROUTES.admin} element={<Dashboard />} />
+        <Route
+          path={APP_ROUTES.admin}
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
         <Route path={APP_ROUTES.login} element={<Login />} />
         <Route path={APP_ROUTES.disclaimer} element={<Disclaimer />} />
         <Route path={APP_ROUTES.contactUs} element={<ContactUs />} />
